d0e4f93a77e586081ae451d146dd285413034550
stateaward <stateaward@naver.com>
2023-03-09 19:21:24 +0900

    🚩 add : insert 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -22,9 +22,26 @@ function App() {
     },
   ]);
 
+  const nextId = useRef(4);
+
+  // 자식에게 전달하는 함수는 useCallback을 써서 리소스 낭비 방지
+  // 자식 컴포넌트에서 onInsert를 실행 -> App.js의 setTodos를 트리거 -> App.js의 state 값 업데이트 -> 재랜더링
+  const onInsert = useCallback((text)=>{
+    console.log('onInsert : ', text);
+    console.log('nextId.current : ', nextId.current);
+    const todo = {
+      id : nextId.current,
+      text,
+      checked: false,
+    };
+    setTodos(todos.concat(todo));
+    nextId.current += 1;
+  }, [todos]);
+
   return (
     <TodoTemplate>
-      <TodoInsert />
+      {/* TodoInsert에게 onInsert 함수를 useCallback으로 감싸서 전달 */}
+      <TodoInsert onInsert={onInsert} />
       {/* 1) TodoList에게 todos라는 이름으로 위 값(상태)를 전송 */}
       <TodoList todos={todos}>Footer 영역</TodoList>
     </TodoTemplate>
--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,10 +1,29 @@
+import {useState, useCallback} from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-export default function TodoInsert() {
+export default function TodoInsert( {onInsert} ) {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback(e => {
+    setValue(e.target.value);
+  }, []);
+
+  // onClick도 가능하지만, submit은 키보드 엔터도 감지함
+  const onSubmit = useCallback((e)=>{
+    onInsert(value);
+    setValue('');
+
+    // submit 고유 동작(브라우저 새로고침) 방지
+    e.preventDefault();
+  }, [onInsert, value]);
+
   return (
-    <form className='TodoInsert'>
-        <input placeholder='할일을 입력하세요' />
+    <form className='TodoInsert' onSubmit={onSubmit}>
+        <input placeholder='할일을 입력하세요'
+               value={value}
+               onChange={onChange}
+        />
         <button type="submit">
             <MdAdd />
         </button>
